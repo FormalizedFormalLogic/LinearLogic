814fe366bf3eb9f3fdc7e7d56697af81b78139d4
palalansouki <palalansouki@gmail.com>
2026-07-22 20:32:16 +0900

    initial commit

---
+++ LinearLogic.lean
@@ -0,0 +1,11 @@
+module  -- shake: keep-all --deprecated_module: ignore
+
+public import LinearLogic.LL.FirstOrder.Calculus
+public import LinearLogic.LL.FirstOrder.ClassicalEmbedding
+public import LinearLogic.LL.FirstOrder.Formula
+public import LinearLogic.LL.FirstOrder.Rew
+public import LinearLogic.LogicSymbol
+public import LinearLogic.MELL.Propositional
+public import LinearLogic.MLL.GoI
+public import LinearLogic.MLL.Propositional
+public import LinearLogic.Vorspiel.GroupTheory.Perm
