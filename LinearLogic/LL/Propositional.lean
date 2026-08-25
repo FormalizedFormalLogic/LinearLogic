a11f236ae41b799f462503acd4fff8dfa737fe1d
palalansouki <palalansouki@gmail.com>
2026-08-26 02:06:20 +0900

    to multiset

--- LinearLogic/LL/Propositional.lean
+++ LinearLogic/LL/Propositional.lean
@@ -2,6 +2,7 @@ module
 
 public import Foundation.Logic.Entailment
 public import LinearLogic.LogicSymbol
+public import LinearLogic.Vorspiel.Multiset
 
 /-!
 # Propositional linear logic without neutrals
@@ -103,16 +104,19 @@ end Formula
 
 variable {α : Type*}
 
-abbrev Sequent := List Formula
+abbrev Sequent := Multiset Formula
 
 namespace Sequent
 
 def IsQuest (Γ : Sequent) : Prop := ∀ A ∈ Γ, Formula.IsQuest A
 
-@[simp] lemma IsQuest.nil : IsQuest [] := by simp [IsQuest]
+@[simp] lemma IsQuest.nil : IsQuest 0 := by simp [IsQuest]
 
 @[simp] lemma IsQuest.cons {A : Formula} {Γ : Sequent} :
-    IsQuest (A :: Γ) ↔ Formula.IsQuest A ∧ IsQuest Γ := by
+    IsQuest (A ::ₘ Γ) ↔ Formula.IsQuest A ∧ IsQuest Γ := by
+  simp [IsQuest]
+
+@[simp] lemma IsQuest.singleton {A : Formula} : IsQuest ⦃A⦄ ↔ A.IsQuest := by
   simp [IsQuest]
 
 end Sequent
@@ -126,26 +130,24 @@ namespace LL
 
 inductive Derivation : Sequent → Type _
   /-- axiom  -/
-  | ax (p : ℕ) : Derivation [.atom p, .natom p]
+  | ax (p : ℕ) : Derivation ⦃.atom p, .natom p⦄
   /-- cut rule -/
-  | cut : Derivation (A :: Γ) → Derivation (∼A :: Δ) → Derivation (Γ ++ Δ)
+  | cut : Derivation (Γ + ⦃A⦄) → Derivation (Δ + ⦃∼A⦄) → Derivation (Γ + Δ)
   /-- structural rules -/
-  | exchange : Derivation Γ → Γ.Perm Δ → Derivation Δ
-  | weakening : Derivation Γ → Derivation (？A :: Γ)
-  | contraction : Derivation (？A :: ？A :: Γ) → Derivation (？A :: Γ)
+  | weakening : Derivation Γ → Derivation (Γ + ⦃？A⦄)
+  | contraction : Derivation (Γ + ⦃？A⦄ + ⦃？A⦄) → Derivation (Γ + ⦃？A⦄)
   /-- multiplicative rules -/
-  | tensor : Derivation (A :: Γ) → Derivation (B :: Δ) → Derivation (A ⨂ B :: (Γ ++ Δ))
-  | par : Derivation (A :: B :: Γ) → Derivation (A ⅋ B :: Γ)
+  | tensor : Derivation (Γ + ⦃A⦄) → Derivation (Δ + ⦃B⦄) → Derivation (Γ + Δ + ⦃A ⨂ B⦄)
+  | par : Derivation (Γ + ⦃A⦄ + ⦃B⦄) → Derivation (Γ + ⦃A ⅋ B⦄)
   /-- additive rules -/
-  | plusLeft : Derivation (A :: Γ) → Derivation (A ⨁ B :: Γ)
-  | plusRight : Derivation (B :: Γ) → Derivation (A ⨁ B :: Γ)
-  | with : Derivation (A :: Γ) → Derivation (B :: Γ) → Derivation (A ＆ B :: Γ)
+  | plusLeft : Derivation (Γ + ⦃A⦄) → Derivation (Γ + ⦃A ⨁ B⦄)
+  | plusRight : Derivation (Γ + ⦃B⦄) → Derivation (Γ + ⦃A ⨁ B⦄)
+  | with : Derivation (Γ + ⦃A⦄) → Derivation (Γ + ⦃B⦄) → Derivation (Γ + ⦃A ＆ B⦄)
   /-- exponential rules -/
-  | dereliction : Derivation (A :: Γ) → Derivation (？A :: Γ)
-  | bang : Derivation (A :: Γ) → Sequent.IsQuest Γ → Derivation (！A :: Γ)
+  | dereliction : Derivation (Γ + ⦃A⦄) → Derivation (Γ + ⦃？A⦄)
+  | bang : Derivation (Γ + ⦃A⦄) → (_ : Sequent.IsQuest Γ := by simp) → Derivation (Γ + ⦃！A⦄)
 
-
-abbrev Proof (A : Formula) : Type _ := Derivation [A]
+abbrev Proof (A : Formula) : Type _ := Derivation ⦃A⦄
 
 instance : Entailment LL Formula := ⟨fun _ ↦ Proof⟩
 
@@ -157,20 +159,31 @@ scoped prefix:45 "⊢ " => Derivable
 
 namespace Derivation
 
-def cast (d : ⊢! Γ) (e : Γ = Δ) : ⊢! Δ := e ▸ d
+def cast (d : ⊢! Γ) (e : Γ = Δ := by abel) : ⊢! Δ := e ▸ d
 
-def rotate (d : ⊢! A :: Γ) : ⊢! Γ ++ [A] :=
-  d.exchange (by grind only [List.perm_comm, List.perm_append_singleton])
+def rotate (d : ⊢! ⦃A⦄ + Γ) : ⊢! Γ + ⦃A⦄ := d.cast
 
-def eta : (A : Formula) → ⊢! [A, ∼A]
+def swap (d : ⊢! ⦃A⦄ + ⦃B⦄) : ⊢! ⦃B⦄ + ⦃A⦄ := d.cast
+
+def cutEq (dA : ⊢! Γ + ⦃A⦄) (dB : ⊢! Δ + ⦃B⦄) (e : B = ∼A := by simp) : ⊢! Γ + Δ :=
+  have : ⊢! Δ + ⦃∼A⦄ := dB.cast (by rw [e])
+  cut dA this
+
+def eta : (A : Formula) → ⊢! ⦃A, ∼A⦄
   |  .atom X => .ax X
-  | .natom X => (ax X).rotate
-  |    A ⨂ B => ((eta A).tensor (eta B)).rotate.par.rotate
-  |    A ⅋ B => ((eta A).rotate.tensor (eta B).rotate).rotate.par
-  |    A ⨁ B => ((eta A).plusLeft.rotate.with (eta B).plusRight.rotate).rotate
-  |   A ＆ B => (eta A).rotate.plusLeft.rotate.with (eta B).rotate.plusRight.rotate
-  |      ！A => (eta A).rotate.dereliction.rotate.bang (by simp)
-  |      ？A => (eta A).dereliction.rotate.bang (by simp) |>.rotate
+  | .natom X => (ax X).swap
+  |    A ⨂ B =>
+    have d : ⊢! ⦃A ⨂ B, ∼A, ∼B⦄ :=
+      ((eta A).swap.tensor (eta B).swap).cast
+    d.par
+  |    A ⅋ B =>
+    have d : ⊢! ⦃∼A ⨂ ∼B, A, B⦄ :=
+      ((eta A).tensor (eta B)).cast
+    d.par.swap
+  |    A ⨁ B => (eta A).swap.plusLeft.swap.with (eta B).swap.plusRight.swap
+  |   A ＆ B => ((eta A).plusLeft.swap.with (eta B).plusRight.swap).swap
+  |      ！A => (eta A).dereliction.swap.bang.swap
+  |      ？A => (eta A).swap.dereliction.swap.bang
 
 end Derivation
 
@@ -178,12 +191,16 @@ namespace Proof
 
 open Derivation
 
-def ax' : 𝐋𝐋⁰ ⊢! A ⊸ A := (eta A).rotate.par
+def ax' : 𝐋𝐋⁰ ⊢! A ⊸ A :=
+  have d : ⊢! ⦃⦄ + ⦃∼A⦄ + ⦃A⦄ := (eta A).swap.cast
+  d.par |>.cast (by simp [Formula.lolli_def])
 
 def modusPonens (d₁ : 𝐋𝐋⁰ ⊢! A ⊸ B) (d₂ : 𝐋𝐋⁰ ⊢! A) : 𝐋𝐋⁰ ⊢! B :=
-  have d₁ : ⊢! [∼(A ⨂ ∼B)] := d₁.cast <| by simp [Formula.lolli_def]
-  have b : ⊢! [A ⨂ ∼B, ∼A, B] := (eta A).tensor (eta B).rotate
-  cut d₂ (cut b d₁)
+  have d₁ : ⊢! ⦃⦄ + ⦃∼(A ⨂ ∼B)⦄ := d₁.cast (by simp [Formula.lolli_def])
+  have b : ⊢! ⦃∼A, B, A ⨂ ∼B⦄ := (eta A).swap.tensor (eta B)
+  have c : ⊢! ⦃∼A, B⦄ := (cut b d₁).cast
+  have d₂ : ⊢! ⦃⦄ + ⦃A⦄ := d₂.cast
+  (cut d₂ c.swap).cast
 
 end Proof
 
---
+++ LinearLogic/Vorspiel/Multiset.lean
@@ -0,0 +1,33 @@
+module
+
+public import Mathlib.Data.Multiset.AddSub
+public import Mathlib.Tactic.Abel
+public import Mathlib.Algebra.Order.Group.Multiset
+
+@[expose] public section
+
+namespace Multiset
+
+/-- Function to avoid reducing `{a} + s` to `a ::ₘ s` -/
+def atom (a : α) : Multiset α := {a}
+
+/-- `⦃x, y, z, ...⦄` notation for `kpair` -/
+syntax "⦃" term,* "⦄" : term
+
+macro_rules
+  | `(⦃$terms:term,*, $term:term⦄) => `(⦃$terms,*⦄ + atom $term)
+  | `(⦃$term:term⦄) => `(atom $term)
+  | `(⦃⦄) => `(0)
+
+@[app_unexpander atom]
+meta def pairUnexpander : Lean.PrettyPrinter.Unexpander
+  | `($_ $term) => `(⦃$term⦄)
+  | _ => throw ()
+
+lemma atom_eq_singleton (a : α) : ⦃a⦄ = {a} := rfl
+
+@[simp] lemma mem_atom_iff {a b : α} : a ∈ ⦃b⦄ ↔ a = b := by simp [atom_eq_singleton]
+
+@[simp] lemma atom_subset_iff {a : α} {s : Multiset α} : ⦃a⦄ ≤ s ↔ a ∈ s := by simp [atom_eq_singleton]
+
+end Multiset
