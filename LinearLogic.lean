bfed16043dc049d0935708d6f508beac4006668e
palalansouki <palalansouki@gmail.com>
2026-08-27 20:59:02 +0900

    add

--- LinearLogic.lean
+++ LinearLogic.lean
@@ -8,8 +8,10 @@ public import LinearLogic.LL.FirstOrder.Calculus
 public import LinearLogic.LL.FirstOrder.ClassicalEmbedding
 public import LinearLogic.LL.FirstOrder.Formula
 public import LinearLogic.LL.FirstOrder.Rew
+public import LinearLogic.LL.Propositional
 public import LinearLogic.LogicSymbol
 public import LinearLogic.MELL.Propositional
 public import LinearLogic.MLL.GoI
 public import LinearLogic.MLL.Propositional
+public import LinearLogic.PhaseSpace.Basic
 public import LinearLogic.Vorspiel.GroupTheory.Perm
